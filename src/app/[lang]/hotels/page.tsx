1c87f06f146a03d8708b1ef396897b0ae1b5bb0a
felicianpopa <felician.popa@bootintech.com>
2025-07-31 13:46:27 +0300

    Add data for hotels

--- src/app/[lang]/hotels/page.tsx
+++ src/app/[lang]/hotels/page.tsx
@@ -1,6 +1,13 @@
 import { getDictionary } from "@/dictionaries/dictionaries";
 
-export default async function Offers({
+// Define the Hotel type based on the API response
+interface Hotel {
+  name: string;
+  rate: string;
+  price: number;
+}
+
+export default async function Hotels({
   params,
 }: {
   params: Promise<{ lang: "en" | "ro" }>;
@@ -8,5 +15,21 @@ export default async function Offers({
   const { lang } = await params;
   const dict = await getDictionary(lang);
 
-  return <h1>{dict.pages.hotels}</h1>;
+  const data = await fetch("http://localhost:5000/hotels");
+  const hotels: Hotel[] = await data.json();
+
+  return (
+    <div className="px-4">
+      <h1>{dict.pages.hotels}</h1>
+      <ul className="grid grid-cols-3 grid-rows-3 gap-4">
+        {hotels.map((hotel: Hotel, index: number) => (
+          <li key={index} className="p-2 mb-2 border-2 rounded-lg">
+            <p>Name: {hotel.name}</p>
+            <p>Rate: {hotel.rate} stars</p>
+            <p>Price: ${hotel.price}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
